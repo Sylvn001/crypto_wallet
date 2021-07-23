afd9042b2529cf3d655c525fa07260ced3835945
Sylvn001 <juniorbaarros@gmail.com>
2021-07-22 22:03:25 -0300

    Created new files using I18n

--- crypto_wallet/app/controllers/welcome_controller.rb
+++ crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails [Cookie]"
+    session[:curso] = "Curso de Ruby on Rails [Session]"
     @nome = params[:nome]
   end
 end
--- crypto_wallet/app/helpers/application_helper.rb
+++ crypto_wallet/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def locale(locale)
+  def locale()
     I18n.locale == :en ? "United States" : "Portugês do Brasil"
   end
 
